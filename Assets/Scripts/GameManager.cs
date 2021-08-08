76274db20211d244a774e78384bb9b4af9d323b6
SakamakiRyu <potesara031w@gmail.com>
2021-08-09 02:20:42 +0900

    GameSceneを更新した

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GamaManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>カウントダウンに使う数字のSprite</summary>
+    [SerializeField] Sprite[] m_nubers;
+    /// <summary>数字を表示するImage</summary>
+    [SerializeField] Image m_displayNumber;
+    /// <summary>カウントダウンオブジェクト</summary>    
+    [SerializeField] GameObject m_countDown;
+    /// <summary>カウントの初期値</summary>
+    [SerializeField] int m_startCount;
+    /// <summary>プレイヤー</summary>
+    [SerializeField] GameObject[] m_players;
+
+    bool m_isStarted = false;
+
+    private void Start()
     {
-        
+        if (m_startCount > m_nubers.Length)
+        {
+            m_startCount = m_nubers.Length - 1;
+        }
+        m_displayNumber.sprite = m_nubers[m_startCount];
+        StartCoroutine(CountDown());
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>カウントダウン</summary>
+    IEnumerator CountDown()
     {
-        
+        for (int i = m_startCount - 1; i > -1; i--)
+        {
+            yield return new WaitForSeconds(1f);
+            m_displayNumber.sprite = m_nubers[i];
+        }
+        m_isStarted = true;
+        Destroy(m_countDown);
     }
 }
